99f61f560b8df183efc446d8948fa7f0bc8f3c7e
parothegreat <alvaroprayogo38@gmail.com>
2026-08-17 15:27:50 +0700

    fix: restore CodeIgniter format compatibility

--- app/Config/Format.php
+++ app/Config/Format.php
@@ -61,4 +61,9 @@ class Format extends BaseConfig
         'application/xml'  => 0,
         'text/xml'         => 0,
     ];
+
+    /**
+     * Maximum depth for JSON encoding.
+     */
+    public int $jsonEncodeDepth = 512;
 }
